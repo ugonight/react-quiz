0492c53026bc14addaa5d1b586049c873d658045
ugonight <amihajime@yahoo.co.jp>
2021-01-14 22:09:26 +0900

    javascriptょくゎかんなぃ…＞＜

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-quiz
 Created with CodeSandbox
+ReactでクイズWebアプリが作りたい（願望）
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import {
   Container,
@@ -7,32 +7,79 @@ import {
   Button
 } from "react-bootstrap";
 
+function Question(props) {
+  return <div className="question">{props.value}</div>;
+}
+
+function Choices(props) {
+  // const [radioValue, setRadioValue] = useState("0");
+
+  const choiceList = ["選択肢1", "選択肢2", "選択肢3"];
+
+  return (
+    <ToggleButtonGroup
+      type="radio"
+      name="options"
+      defaultValue={1}
+      vertical
+      className="choices"
+    >
+      {choiceList.map((choice, idx) => (
+        <ToggleButton
+          key={idx}
+          variant="outline-dark"
+          className="choice mt-2"
+          name="radio"
+          value={idx}
+          // checked={radioValue === idx}
+          onChange={(e) => props.onChange(e.currentTarget.value)}
+        >
+          {choice}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  );
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: 0
+    };
+  }
+
+  changeInput(i) {
+    this.setState({
+      input: i
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Question value="問題" />
+
+        <Choices onChange={(i) => this.changeInput(i)} />
+
+        <Button
+          variant="primary"
+          size="lg"
+          block
+          className="mt-4"
+          onClick={(i) => alert(this.state.input)}
+        >
+          決定
+        </Button>
+      </>
+    );
+  }
+}
+
 function App() {
   return (
     <Container>
-      <div className="question">問題</div>
-
-      <ToggleButtonGroup
-        type="radio"
-        name="options"
-        defaultValue={1}
-        vertical
-        className="choices"
-      >
-        <ToggleButton value={1} variant="outline-dark" className="choice mt-2">
-          選択肢1
-        </ToggleButton>
-        <ToggleButton value={2} variant="outline-dark" className="choice mt-2">
-          選択肢2
-        </ToggleButton>
-        <ToggleButton value={3} variant="outline-dark" className="choice mt-2">
-          選択肢3
-        </ToggleButton>
-      </ToggleButtonGroup>
-
-      <Button variant="primary" size="lg" block className="mt-4">
-        決定
-      </Button>
+      <Game />
     </Container>
   );
 }
